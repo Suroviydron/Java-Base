622082c4a96aa9cb6eb7c038eb8c1ded4112a8dc
SuroviyDron <ablezgov@inbox.ru>
2020-04-03 11:02:56 +0300

    HomeWork01

---
+++ HW01.js
@@ -0,0 +1,19 @@
+//Task 1
+var tempCel = parseInt(prompt("Celsus: "));
+
+var tempFar = 1.8 * tempCel + 32;
+
+alert("Farengeit: " + tempFar);
+
+//Task 3
+var admin, name;
+
+name = "Vasiliy";
+
+admin = name;
+
+alert(admin);
+
+//Task 4
+
+document.write(1000 + "108");
